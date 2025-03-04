27a35e1eaa48dc65307a22aff571e6d114ab4f5d
Nitin kumar Sahu <65076355+Nitin-sahu@users.noreply.github.com>
2025-03-04 13:16:51 +0530

    Add files via upload

---
+++ Project 2 _social media operator_ social media analysis/to test a file for opening using os library.py
@@ -0,0 +1,5 @@
+import os
+
+file_path = "mobile_usage_behavioral_analysis"
+os.startfile(r"C:\Users\HP\Desktop\python project\Project 2\mobile_usage_behavioral_analysis.csv");
+
---
+++ project 1_Insurance agent _Estimation of premium that a customer shouold be charged ml model on data analysis/Tuitor drive import os.py
@@ -0,0 +1,2 @@
+import os
+print(os.path.exists(r"C:\Users\HP\Desktop\python project\ML Algorithms with Python Assignment (Data) (1).csv"))
